d5b656f3490e17117c8ee27e820bc50f81e2f851
GentaArya <mgentaarya@yahoo.com>
2025-01-24 15:51:48 +0700

    fix

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ export const sendNotificationEmail = (emailList, existingPkl) => {
 
 export const io = new SocketIOServer(httpServer, {
   cors: {
-    origin: ["http://localhost:5173", "http://siabsen.apiservices.my.id"],
+    origin: ["http://localhost:5173", "https://siabsen.apiservices.my.id"],
     methods: ["GET", "POST"],
     credentials: true,
   },

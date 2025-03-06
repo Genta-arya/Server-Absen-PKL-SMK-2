eb6f4bd9f4c1de4b4575f9c1b2e7c16bac3d9e24
GentaArya <mgentaarya@yahoo.com>
2025-03-06 13:44:05 +0700

    push

--- index.js
+++ index.js
@@ -32,10 +32,19 @@ const app = express();
 const PORT = process.env.PORT;
 const httpServer = createServer(app);
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://monitoring.smkn2ketapang.sch.id",
+  "https://digital.smkn2ketapang.sch.id",
+  "https://digital-tester.smkn2ketapang.sch.id",
+];
+
 app.use(cookieParser());
 const io = new Server(httpServer, {
   cors: {
-    origin: "[http://localhost:5174 ,http://localhost:5173 , https://monitoring.smkn2ketapang.sch.id]",
+    origin: allowedOrigins,
+
     methods: ["GET", "POST"],
   },
 });
@@ -92,13 +101,6 @@ app.set("trust proxy", 1);
 
 // app.use(limiter);
 app.use(express.json({ limit: "150mb" }));
-
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://monitoring.smkn2ketapang.sch.id",
-  "https://digital.smkn2ketapang.sch.id",
-  "https://digital-tester.smkn2ketapang.sch.id",
-];
 
 app.use((req, res, next) => {
   const origin = req.headers.origin;
